15d16c5398deec7674f366e6767d171df22af59e
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-07 14:48:48 +0300

    ✨ feat(icons): add various icon size constants

---
+++ src/components/IconSize/index.tsx
@@ -0,0 +1,29 @@
+export const IconExtraSmall = {
+  width: 16,
+  height: 16,
+};
+
+export const IconSmall = {
+  width: 32,
+  height: 32,
+};
+
+export const IconMedium = {
+  width: 64,
+  height: 64,
+};
+
+export const IconLarge = {
+  width: 128,
+  height: 128,
+};
+
+export const IconExtraLarge = {
+  width: 256,
+  height: 256,
+};
+
+export const IconDoubleExtraLarge = {
+  width: 512,
+  height: 512,
+};
