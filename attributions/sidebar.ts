5e7685b81290dfd440eb7638716332fd88f8b05d
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-07 14:46:54 +0300

    ✨ feat(attributions): add attributions page and sidebar entry

---
+++ attributions/sidebar.ts
@@ -0,0 +1,17 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  seriesSidebar: [
+    {
+      type: "category",
+      label: "Attributions",
+      link: {
+        type: "doc",
+        id: "index",
+      },
+      items: [],
+    },
+  ],
+};
+
+export default sidebar;
