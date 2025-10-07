b87b354a379178b31bc8be88074c91425f9f317c
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-07 14:10:54 +0300

    ✨ feat(emoji): add Noto emoji (Unicode 17) SVG imports and exports

---
+++ src/assets/noto-emoji-unicode-17/index.tsx
@@ -0,0 +1,101 @@
+import AlienMonster from "./emoji_u1f47e.svg";
+import ArtistDefault from "./emoji_u1f9d1_200d_1f3a8.svg";
+import ArtistPalette from "./emoji_u1f3a8.svg";
+import BookmarkTabs from "./emoji_u1f4d1.svg";
+import Books from "./emoji_u1f4da.svg";
+import Brain from "./emoji_u1f9e0.svg";
+import CardIndexDividers from "./emoji_u1f5c2.svg";
+import Cloud from "./emoji_u2601.svg";
+import CrystalBall from "./emoji_u1f52e.svg";
+import DesktopComputer from "./emoji_u1f5a5.svg";
+import DetectiveDefault from "./emoji_u1f575.svg";
+import Dizzy from "./emoji_u1f4ab.svg";
+import Email from "./emoji_u1f4e7.svg";
+import GameDie from "./emoji_u1f3b2.svg";
+import GlobeWithMeridians from "./emoji_u1f310.svg";
+import GlowingStar from "./emoji_u1f31f.svg";
+import HammerAndWrench from "./emoji_u1f6e0.svg";
+import Handshake from "./emoji_u1f91d.svg";
+import Herb from "./emoji_u1f33f.svg";
+import HighVoltage from "./emoji_u26a1.svg";
+import Laptop from "./emoji_u1f4bb.svg";
+import LightBulb from "./emoji_u1f4a1.svg";
+import Memo from "./emoji_u1f4dd.svg";
+import PageFacingUp from "./emoji_u1f4c4.svg";
+import Penguin from "./emoji_u1f427.svg";
+import PersonDefault from "./emoji_u1f9d1.svg";
+import PoliceCarLight from "./emoji_u1f6a8.svg";
+import Pushpin from "./emoji_u1f4cc.svg";
+import PuzzlePiece from "./emoji_u1f9e9.svg";
+import Radioactive from "./emoji_u2622.svg";
+import Robot from "./emoji_u1f916.svg";
+import Rocket from "./emoji_u1f680.svg";
+import SatelliteAntenna from "./emoji_u1f4e1.svg";
+import Seal from "./emoji_u1f9ad.svg";
+import SmilingFaceWithSmilingEyes from "./emoji_u1f60a.svg";
+import Snake from "./emoji_u1f40d.svg";
+import Sparkles from "./emoji_u2728.svg";
+import SpiderWeb from "./emoji_u1f578.svg";
+import SpiralShell from "./emoji_u1f41a.svg";
+import SpoutingWhale from "./emoji_u1f433.svg";
+import Starstruck from "./emoji_u1f929.svg";
+import TechnologistDefault from "./emoji_u1f9d1_200d_1f4bb.svg";
+import TeddyBear from "./emoji_u1f9f8.svg";
+import Telephone from "./emoji_u260e.svg";
+import Toolbox from "./emoji_u1f9f0.svg";
+import Trophy from "./emoji_u1f3c6.svg";
+import VideoGame from "./emoji_u1f3ae.svg";
+import Whale from "./emoji_u1f40b.svg";
+import WritingHandDefault from "./emoji_u270d.svg";
+
+export {
+  AlienMonster,
+  ArtistDefault,
+  ArtistPalette,
+  BookmarkTabs,
+  Books,
+  Brain,
+  CardIndexDividers,
+  Cloud,
+  CrystalBall,
+  DesktopComputer,
+  DetectiveDefault,
+  Dizzy,
+  Email,
+  GameDie,
+  GlobeWithMeridians,
+  GlowingStar,
+  HammerAndWrench,
+  Handshake,
+  Herb,
+  HighVoltage,
+  Laptop,
+  LightBulb,
+  Memo,
+  PageFacingUp,
+  Penguin,
+  PersonDefault,
+  PoliceCarLight,
+  Pushpin,
+  PuzzlePiece,
+  Radioactive,
+  Robot,
+  Rocket,
+  SatelliteAntenna,
+  Seal,
+  SmilingFaceWithSmilingEyes,
+  Snake,
+  Sparkles,
+  SpiderWeb,
+  SpiralShell,
+  SpoutingWhale,
+  Starstruck,
+  TechnologistDefault,
+  TeddyBear,
+  Telephone,
+  Toolbox,
+  Trophy,
+  VideoGame,
+  Whale,
+  WritingHandDefault,
+};
